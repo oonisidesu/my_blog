1607078d26c16bfc6389780cde319c5f9c362b18
oonisidesu <eeeelllleeee24@gmail.com>
2018-08-26 11:07:36 +0900

    blog編集リファクタリングとか

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
+before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result.page(params[:page]).per(3)
+    @new_posts = Post.find_newest_article
+    @author = Author.first
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -17,19 +22,23 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :body, :category)
